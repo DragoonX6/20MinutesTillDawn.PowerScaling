b5c7b6f0106e4ffd2f1b92ba8ee74e43e5b750db
Dragoon <DragoonX6@users.noreply.github.com>
2022-11-18 12:03:54 +0100

    Cap dodge to 70%

--- 20MinutesTillDawn.PowerScaling/CapStats.cs
+++ 20MinutesTillDawn.PowerScaling/CapStats.cs
@@ -1,4 +1,6 @@
 using HarmonyLib;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
 
 using flanne;
 
@@ -15,5 +17,20 @@ public static class CapStats
 	static void CapProjectileSpeed(ref ProjectileRecipe __result)
 	{
 		__result.projectileSpeed = Mathf.Min(__result.projectileSpeed, 100f);
+	}
+
+	// Caps dodge to 70%
+	[HarmonyPatch(typeof(DodgeRoller), nameof(DodgeRoller.Roll))]
+	[HarmonyILManipulator]
+	static void ManipulateRoll(ILContext il)
+	{
+		ILCursor c = new(il);
+
+		c.GotoNext(
+			MoveType.Before,
+			x => x.Match(OpCodes.Ldloc_0),
+			x => x.MatchLdcR4(0.0f));
+
+		c.RemoveRange(16);
 	}
 }
