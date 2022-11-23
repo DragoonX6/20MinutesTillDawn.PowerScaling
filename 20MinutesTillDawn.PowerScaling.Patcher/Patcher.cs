1b1cb520d0aaf215d8da002c36fd2c8aadac311f
Dragoon <DragoonX6@users.noreply.github.com>
2022-11-23 16:05:47 +0100

    Make sure PlayLevelUpAnimationCR is not inlined

--- 20MinutesTillDawn.PowerScaling.Patcher/Patcher.cs
+++ 20MinutesTillDawn.PowerScaling.Patcher/Patcher.cs
@@ -1,4 +1,8 @@
 using System.Collections.Generic;
+using System.Linq;
+
+using BepInEx.Logging;
+
 using Mono.Cecil;
 
 namespace _20MinutesTillDawn.PowerScaling.Patcher;
@@ -8,7 +12,25 @@ public static class Patcher
 	public static IEnumerable<string> TargetDlls { get; } =
 		new[]{"Assembly-CSharp.dll"};
 
+	private static ManualLogSource log = null;
+
+	public static void Initialize()
+	{
+		log = Logger.CreateLogSource("20MinutesTillDawn.PowerScaling.Patcher");
+	}
+
 	public static void Patch(AssemblyDefinition assembly)
 	{
+		TypeDefinition powerupMenuStatetype = assembly.MainModule.Types
+			.Where(t => t.Name.Contains("PowerupMenuState"))
+			.First();
+
+		MethodDefinition method = powerupMenuStatetype.Methods
+			.Where(t => t.Name.Contains("PlayLevelUpAnimationCR"))
+			.First();
+
+		method.NoInlining = true;
+
+		log.LogInfo("Added `noinlining` attribute to `PlayLevelUpAnimationCR`");
 	}
 }
